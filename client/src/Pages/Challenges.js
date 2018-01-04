c548f5bac6cff4157c50234f6c80ccea4d592fa6
Joy Hauser <joy.hauser@hotmail.com>
2018-01-04 00:19:21 -0600

    Add logic to toggle card details when card header is clicked

--- client/src/Pages/Challenges.js
+++ client/src/Pages/Challenges.js
@@ -1,16 +1,35 @@
 import React, { Component } from 'react';
-import { PanelGroup, Panel } from 'react-bootstrap';
+import { PanelGroup, Panel, Button } from 'react-bootstrap';
 
 class Challenges extends Component {
-	state = {challenges: []}
+	constructor(props) {
+		super(props);
+		this.state = {
+			opened: false,
+			challenges: []
+		};
+		this.showDetails = this.showDetails.bind(this);
+	}
 
 	componentDidMount() {
 		fetch('/challenges')
 		.then(res => res.json())
 		.then(challenges => this.setState({ challenges }));
 	}
-	
+
+	showDetails() {
+		console.log("State");
+		console.log(this.state);
+		const { opened } = this.state;
+		this.setState({
+			// toggle value of `opened`
+			opened: !opened,
+		});
+	}
+
 	render() {
+		const { opened } = this.state;
+
 		return (
 			<div className="Challenges">
 				<main>
@@ -19,28 +38,30 @@ class Challenges extends Component {
 				<p className="Challenges-intro">
 					This is the  information about how you are doing on each of the challenges.
 				</p>
-				
+
 				<div className="card-container">
 					{this.state.challenges.map(challenge =>
 						<div className="card">
-							<div className="card-header">
+							<div className="card-header" onClick={this.showDetails}>
 								<div className="card-left">
 									{challenge.name}
 								</div>
 							</div>
-							<div className="card-details">
-								<div className="card-left">
-									Flag:  
-									<input type="text" value={challenge.answer} />
-									<button>
-										Check
-									</button>
+							{opened && (
+								<div className="card-details">
+									<div className="card-left">
+										Flag:
+										<input type="text" defaultValue={challenge.answer} />
+										<Button>
+											Check
+										</Button>
+									</div>
+									<br/>
+									<div className="card-left">
+										Clue: {challenge.clue}
+									</div>
 								</div>
-								<br/>
-								<div className="card-left">
-									Clue: {challenge.clue}
-								</div>
-							</div>
+							)}
 						</div>
 					)}
 				</div>
