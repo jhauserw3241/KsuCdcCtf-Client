fbf513215b7d8918af03edf78df1c1b912faa971
Joy Hauser <joy.hauser@hotmail.com>
2018-01-03 23:17:53 -0600

    Add custom cards for challenges page

--- client/src/Pages/Challenges.js
+++ client/src/Pages/Challenges.js
@@ -19,15 +19,31 @@ class Challenges extends Component {
 				<p className="Challenges-intro">
 					This is the  information about how you are doing on each of the challenges.
 				</p>
-
-				<PanelGroup>
+				
+				<div className="card-container">
 					{this.state.challenges.map(challenge =>
-						<Panel collapsible header={challenge.name} eventKey={challenge.id}>
-							Answer: {challenge.answer}<br/>
-							Clue: {challenge.clue}
-						</Panel>
+						<div className="card">
+							<div className="card-header">
+								<div className="card-left">
+									{challenge.name}
+								</div>
+							</div>
+							<div className="card-details">
+								<div className="card-left">
+									Flag:  
+									<input type="text" value={challenge.answer} />
+									<button>
+										Check
+									</button>
+								</div>
+								<br/>
+								<div className="card-left">
+									Clue: {challenge.clue}
+								</div>
+							</div>
+						</div>
 					)}
-				</PanelGroup>
+				</div>
 			</div>
 		);
 	}
