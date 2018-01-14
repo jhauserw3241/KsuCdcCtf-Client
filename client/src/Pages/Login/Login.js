197318a9fa18ab9c3d5c9027d5b95db0afdc3696
Joy Hauser <joy.hauser@hotmail.com>
2018-01-13 22:56:04 -0600

    Update login page to allow the user to communicate with the server

--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -1,21 +1,49 @@
 import React, { Component } from 'react';
-import UsernameElement from './UsernameElement';
-import PasswordElement from './PasswordElement';
-import SubmitElement from './SubmitElement';
+import { Button } from 'react-bootstrap';
+import './../../CSS/Login.css';
 
 class Login extends Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			username: "",
+			password: ""
+		};
+	}
+
+	login(username, password) {
+		console.log("Username: " + this.state.username);
+		console.log("Password: " + this.state.password);
+		
+		fetch(`/login/${username}&${password}`)
+		.then((response) => response.json())
+		.then((data) =>
+		{
+			console.log("Got response");
+			console.log(data.success);
+		})
+	}
+	
 	render() {
 		return (
-			<div className="Login">
+			<div className="Login form-container">
 				<main>
 					{this.props.children}
 				</main>
 				
-				<UsernameElement />
+				<div className="left form-row">
+					Username: 
+					<input type="text" value={this.state.username} onChange={(event) => {this.setState({username: event.target.value})}} />
+				</div>
 				<br/>
-				<PasswordElement />
+				<div className="left form-row">
+					Password: 
+					<input type="text" value={this.state.password} onChange={(event) => {this.setState({password: event.target.value})}} />
+				</div>
 				<br/>
-				<SubmitElement />
+				<Button className="left form-row submit-button" onClick={()=>this.login(this.state.username, this.state.password)}>
+					Submit
+				</Button>
 			</div>
 		);
 	}
