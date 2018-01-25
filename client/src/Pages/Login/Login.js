7a1ac4dfc0508bd0d01535ab438d0a6a2cdae602
Joy Hauser <joy.hauser@hotmail.com>
2018-01-25 14:39:56 -0600

    Fix login page error

--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-//import  { Redirect } from 'react-router-dom';
+import  { Redirect } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
 import './../../CSS/Login.css';
 
