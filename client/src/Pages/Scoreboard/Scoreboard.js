c344362e566d3a7faf9fc7e5f56ce09b207c6cf7
Joy Hauser <joy.hauser@hotmail.com>
2018-02-27 17:36:29 -0600

    Decrease size of scoreboard cards

--- client/src/Pages/Scoreboard/Scoreboard.js
+++ client/src/Pages/Scoreboard/Scoreboard.js
@@ -2,15 +2,29 @@ import React, { Component } from 'react';
 import ScoreboardElement from './ScoreboardElement';
 
 class Scoreboard extends Component {
-	state = {people: []}
+	constructor(props) {
+		super(props);
+
+		this.state = {
+			people: [],
+			num_people: -1
+		};
+	}
 
 	componentDidMount() {
 		fetch('/scoreboard')
 		.then(res => res.json())
-		.then(people => this.setState({ people }));
+		.then(people => this.setState({
+			people: people,
+			num_people: people.length
+		}));
 	}
 	
 	render() {
+		var midpoint = this.state.num_people / 2;
+		var first_half_people = this.state.people.slice(0, midpoint);
+		var second_half_people = this.state.people.slice(midpoint, this.state.num_people);
+
 		return (
 			<div className="Scoreboard">
 				<main>
@@ -22,9 +36,16 @@ class Scoreboard extends Component {
 				</p>
 
 				<div className="list-container">
-					{this.state.people.map(person =>
-						<ScoreboardElement key={person.eid} eid={person.eid} score={person.score} />
-					)}
+					<div className="list-column">
+						{first_half_people.map(person =>
+							<ScoreboardElement key={person.eid} eid={person.eid} score={person.score} />
+						)}
+					</div>
+					<div className="list-column">
+						{second_half_people.map(person =>
+							<ScoreboardElement key={person.eid} eid={person.eid} score={person.score} />
+						)}
+					</div>
 				</div>
 			</div>
 		);
