e3deb4c2c5db22843a30e442344f9efd72f3514e
Joy Hauser <joy.hauser@hotmail.com>
2018-02-27 20:59:12 -0600

    Remove button requirements for Challenges and Login page

--- client/src/Pages/Login/Login.js
+++ client/src/Pages/Login/Login.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import  { Redirect } from 'react-router-dom';
-import { Button } from 'react-bootstrap';
 import './../../CSS/Login.css';
 
 class Login extends Component {
